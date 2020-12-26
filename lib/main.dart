a46b6efe0599b7216f5164336663cd1225247389
awesometoast357 <awesometoast357@gmail.com>
2020-12-26 20:52:29 +0800

    Laptop ran into some problems, gonna push for saftey measures

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import 'package:final_project/screens/DashboardScreen.dart';
 import 'package:final_project/screens/NotepadScreen.dart';
 import 'package:flutter/material.dart';
 import './screens/NotepadForm.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  //initializes the firebase addon
+  await Firebase.initializeApp();
+
   runApp(
     MaterialApp(
-      home: NotepadScreen(),
+      home: AuthenticationWrapper(),
     ),
   );
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
--- lib/screens/NotepadScreen.dart
+++ lib/screens/NotepadScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './DashboardScreen.dart';
 
 class NotepadScreen extends StatefulWidget {
   @override
@@ -10,6 +11,10 @@ class _NotepadScreenState extends State<NotepadScreen> {
   final _formKey = GlobalKey<FormState>();
 
   Widget build(BuildContext context) {
+    Navigator.pop(
+      context,
+      DashboardScreen(),
+    );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
@@ -18,10 +23,7 @@ class _NotepadScreenState extends State<NotepadScreen> {
       body: SingleChildScrollView(
         child: Form(
           key: _formKey,
-          child: Container(
-            padding: EdgeInsets.all(15),
-            child: new NoteFormFieldBuilder(),
-          ),
+          child: TextField(),
         ),
       ),
     );
@@ -42,59 +44,17 @@ class NoteFormFieldBuilder extends StatelessWidget {
     final messageController = TextEditingController(text: message);
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          TextFormField(
-              controller: titleController,
-              decoration: const InputDecoration(
-                  hintText: 'Title Here', labelText: 'Title'),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return "Enter text here";
-                }
-                return "Text here please";
-              }),
-          TextFormField(
-            controller: messageController,
-            decoration: const InputDecoration(
-                hintText: 'Talk about something!', labelText: 'Message'),
-            validator: (value) {
-              if (value.isEmpty) {
-                return "Please enter some text";
-              }
-              return null;
-            },
-            maxLines: null,
-            keyboardType: TextInputType.multiline,
-          ),
-          RaisedButton(
-            child: Text('Submit'),
-            onPressed: () async {
-              //final notesModel = context.read<NoteModel>();
-
-              String title = titleController.text;
-              String message = messageController.text;
-
-              bool isCreating; //= (note?.id == null);
-
-              if (isCreating) {
-                //notesModel.createNewNote({"title": title, "message": message});
-              } else {
-                /*
-              Notes activeNotes =
-                  Provider.of<NoteModel>(context, listen: false).getActiveNotes;
-
-              activeNotes.title = title;
-              activeNotes.message = message;
-
-              await Provider.of<NoteModel>(context, listen: false)
-                  .saveActiveNoteEdits();
-              */
-              }
-              Navigator.pop(context);
-            },
-          ),
-        ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            padding: EdgeInsets.all(15),
+            child: ConstrainedBox(
+              constraints: new BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                  minWidth: constraints.maxWidth),
+            ),
+          );
+        },
       ),
     );
   }
